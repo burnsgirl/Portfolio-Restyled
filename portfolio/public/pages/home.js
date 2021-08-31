f61f6e78b6b887cdfa1c6e89a974f277af5546a6
Dauphine Burns <dauphineburns@gmail.com>
2021-08-31 10:45:22 -0400

    home stylesheetcreated

---
+++ portfolio/public/pages/home.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+function About () {
+    return (
+        <div class="container">
+            <div class="cards">
+
+                <div id="recipegram">
+                    <div class="overview" id="recipegram-overview">
+                    <div class="mockup" id="recipegram-mockup">
+                        <img>
+                    <div class="links">
+                        <a href="https://recipe-gram-best-team.herokuapp.com/">Deployed</a>
+                        <a href="https://github.com/burnsgirl/Recipe-Gram">GitHub</a>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+
+                <div id="codequiz">
+                    <div class="overview" id="codequiz-overview">
+                    <div class="mockup" id="codequiz-mockup">
+                        <img>
+                    <div class="links">
+                        <a href="https://burnsgirl.github.io/Code-Quiz/">Deployed</a>
+                        <a href="https://github.com/burnsgirl/Code-Quiz">GitHub</a>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+
+                <div id="workschedule">
+                    <div class="overview" id="workschedule-overview">
+                    <div class="mockup" id="workschedule-mockup">
+                        <img>
+                    <div class="links">
+                        <a href="https://burnsgirl.github.io/Work-Schedule/">Deployed</a>
+                        <a href="https://github.com/burnsgirl/Work-Schedule">GitHub</a>
+                    </div>
+                    </div>
+                    </div>
+                </div> 
+
+            </div>
+        </div>
+    );
+}
+
+
+export default About;
